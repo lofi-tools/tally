f2c8d79ca78bcf3065386e978ffcd7f0812c7a90
Nicolas M <marshall.nicolas@gmail.com>
2026-07-22 10:23:07 +0100

    flake: wip ixbrl-reporter

--- flake.nix
+++ flake.nix
@@ -1,72 +1,7 @@
 {
-  # inputs = {
-  #   nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-  #   utils.url = "github:numtide/flake-utils";
-  #   rust-overlay.url = "github:oxalica/rust-overlay";
-  #   my-utils = {
-  #     url = "github:nmrshll/nix-utils";
-  #     inputs.nixpkgs.follows = "nixpkgs";
-  #     inputs.utils.follows = "utils";
-  #   };
-  # };
   inputs.nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
   inputs.parts.url = "github:hercules-ci/flake-parts";
   inputs.my-nix = { url = "github:nmrshll/my-nix"; inputs.nixpkgs.follows = "nixpkgs"; inputs.fp.follows = "parts"; };
-
-  # outputs = { self, nixpkgs, utils, rust-overlay, my-utils }:
-  #   with builtins; utils.lib.eachDefaultSystem (system:
-  #     let
-  #       pkgs = import nixpkgs {
-  #         inherit system;
-  #         overlays = [ (import rust-overlay) ];
-  #       };
-  #       customRust = pkgs.rust-bin.stable."1.86.0".default.override {
-  #         extensions = [ "rust-src" "rust-analyzer" ];
-  #         targets = [ ];
-  #       };
-
-  #       baseInputs = [
-  #         customRust
-  #         pkgs.pkg-config
-  #       ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-  #         pkgs.apple-sdk_15
-  #         pkgs.libiconv
-  #       ];
-
-  #       devInputs = with pkgs; [
-  #         nixpkgs-fmt
-  #         cargo-nextest
-  #         # cargo-watch
-  #         # cargo-edit
-  #       ];
-
-  #       env = {
-  #         RUST_BACKTRACE = "1";
-  #         CARGO_NET_GIT_FETCH_WITH_CLI = "true";
-  #       };
-
-  #       scripts = mapAttrs (name: value: pkgs.writeShellScriptBin name value) {
-  #         run = ''cargo run -- "$@" '';
-  #         packages = ''if [ -n "$CRATE" ]; then echo "-p $CRATE"; else echo "--workspace"; fi '';
-  #         # utest = ''cargo nextest run --workspace -E '!test(nordigen)' --nocapture -- $SINGLE_TEST '';
-  #         utest = ''set -x; cargo nextest run $(packages) -E "''${TEST_FILTER:-all()}" --nocapture "$@" -- $SINGLE_TEST '';
-  #         ftest = ''set -x; cargo nextest run --workspace -E "''${TEST_FILTER:-all()}" --nocapture "$@" '';
-  #         backup-txs = ''mkdir -p ./.cache/backup && mv ./.cache/starling_transactions.json ./.cache/backup/starling_transactions.$(date +%Y%m%d%H%M).json'';
-  #       };
-
-  #     in
-  #     {
-  #       devShells.default = with pkgs; mkShell {
-  #         inherit env;
-  #         buildInputs = baseInputs ++ devInputs ++ (attrValues scripts);
-  #         shellHook = "
-  #             ${my-utils.binaries.${system}.configure-vscode};
-  #             dotenv
-  #           ";
-  #       };
-  #     }
-  #   );
-
 
   outputs = inputs@{ parts, ... }: parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: with builtins; {
     systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
@@ -75,7 +10,7 @@
       inputs.my-nix.flakeModules.rust
       (inputs.my-nix.lib.findFlakePartFilesRec ./.)
     ];
-    perSystem = { pkgs, l, system, ... }:
+    perSystem = { pkgs, system, ... }:
       let
         buildTimeDeps = [
           pkgs.pkg-config
@@ -85,41 +20,90 @@
           pkgs.cargo-nextest
         ];
 
+        ownPkgs.piecash = pkgs.python3Packages.buildPythonPackage rec {
+          pname = "piecash";
+          version = "1.2.0";
+          format = "setuptools";
+
+          src = pkgs.fetchPypi {
+            inherit pname version;
+            hash = "sha256-iWOfBmHUkiQng/OcjRR+pFwyHcQRH5PsopefBw9fF20=";
+          };
+
+          propagatedBuildInputs = [
+            pkgs.python3Packages.sqlalchemy
+            pkgs.python3Packages.pyyaml
+          ];
+
+          pythonImportsCheck = [ "piecash" ];
+
+          meta = {
+            description = "Python library for GnuCash file access";
+            homepage = "https://github.com/sdementen/piecash";
+            license = lib.licenses.gpl2Plus;
+          };
+        };
+
+        ownPkgs.ixbrl-parse = pkgs.python3Packages.buildPythonPackage rec {
+          pname = "ixbrl-parse";
+          version = "0.3";
+          format = "setuptools";
+
+          src = pkgs.fetchPypi {
+            inherit pname version;
+            hash = "";
+          };
+
+          propagatedBuildInputs = [
+            pkgs.python3Packages.lxml
+          ];
+
+          pythonImportsCheck = [ "ixbrl" ];
+
+          meta = {
+            description = "Python iXBRL parser";
+            homepage = "https://github.com/cybermaggedon/ixbrl-parse";
+            license = lib.licenses.gpl3Plus;
+          };
+        };
+
         ownPkgs.ixbrl-reporter =
-          let
-            versions."1.2.1" = { rev = "a0ec37f"; sha256 = "sha256-AMAO3ygDiIVkCsHmHy1fdGp4CVgb7YRV1M8w1mymUhY="; };
-            mkPkg = { version ? l.latest versions, ... }:
-              pkgs.python3Packages.buildPythonPackage rec {
-                pname = "ixbrl-reporter";
-                inherit version;
-                format = "pyproject";
-                src = pkgs.fetchFromGitHub {
-                  owner = "cybermaggedon";
-                  repo = "ixbrl-reporter";
-                  rev = versions.${version}.rev;
-                  sha256 = versions.${version}.sha256;
-                };
-                propagatedBuildInputs = [
-                  pkgs.python3Packages.requests
-                  pkgs.python3Packages.lxml
-                  # pkgs.python3Packages.piecash
-                  pkgs.python3Packages.pyyaml
-                ];
-                nativeBuildInputs = [
-                  pkgs.python3Packages.pytest
-                  pkgs.python3Packages.pytest-cov
-                  pkgs.python3Packages.pytest-mock
-                ];
-                pythonImportsCheck = [ "ixbrl_reporter" ];
-                meta = {
-                  description = "Production of iXBRL reports from templates and accounts files";
-                  homepage = "https://github.com/cybermaggedon/ixbrl-reporter";
-                  license = lib.licenses.gpl3Plus;
-                  platforms = lib.platforms.unix;
-                };
-              };
-          in
-          mkPkg { };
+          pkgs.python3Packages.buildPythonPackage rec {
+            pname = "ixbrl-reporter";
+            version = "1.2.1";
+            format = "pyproject";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "cybermaggedon";
+              repo = "ixbrl-reporter";
+              rev = "v${version}";
+              hash = "sha256-AMAO3ygDiIVkCsHmHy1fdGp4CVgb7YRV1M8w1mymUhY=";
+            };
+
+            propagatedBuildInputs = [
+              pkgs.python3Packages.requests
+              pkgs.python3Packages.lxml
+              ownPkgs.piecash
+              pkgs.python3Packages.pyyaml
+            ];
+
+            nativeBuildInputs = [
+              pkgs.python3Packages.setuptools
+              pkgs.python3Packages.pytest
+              pkgs.python3Packages.pytest-cov
+              pkgs.python3Packages.pytest-mock
+              ownPkgs.ixbrl-parse
+            ];
+
+            pythonImportsCheck = [ "ixbrl_reporter" ];
+
+            meta = {
+              description = "Production of iXBRL reports from templates and accounts files";
+              homepage = "https://github.com/cybermaggedon/ixbrl-reporter";
+              license = lib.licenses.gpl3Plus;
+              platforms = lib.platforms.unix;
+            };
+          };
 
         bash.wd = "$(git rev-parse --show-toplevel)";
         bin = inputs.my-nix.bin.${system} // (mapAttrs (n: p: "${p}/bin/${n}") scripts) // {
@@ -128,7 +112,6 @@
         scripts = mapAttrs (n: s: pkgs.writeShellScriptBin n s) {
           run = ''cargo run -- "$@" '';
           packages = ''if [ -n "$CRATE" ]; then echo "-p $CRATE"; else echo "--workspace"; fi '';
-          # utest = ''cargo nextest run --workspace -E '!test(nordigen)' --nocapture -- $SINGLE_TEST '';
           utest = ''set -x; cargo nextest run $(packages) -E "''${TEST_FILTER:-all()}" --nocapture "$@" -- $SINGLE_TEST '';
           ftest = ''set -x; cargo nextest run --workspace -E "''${TEST_FILTER:-all()}" --nocapture "$@" '';
           backup-txs = ''mkdir -p ./.cache/backup && mv ./.cache/starling_transactions.json ./.cache/backup/starling_transactions.$(date +%Y%m%d%H%M).json'';
@@ -136,12 +119,11 @@
         };
 
         env = {
-          # RUST_BACKTRACE = "1";
           CARGO_NET_GIT_FETCH_WITH_CLI = "true";
         };
       in
       {
-        packages = scripts;
+        packages = scripts // ownPkgs;
         rust.buildInputs = runtimeDeps;
         rust.nativeBuildInputs = buildTimeDeps;
         rust.buildEnv = env;
