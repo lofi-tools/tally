a82a3c337ba1a58b73bc357ec88802047b506412
Nicolas M <marshall.nicolas@gmail.com>
2026-07-22 08:52:05 +0100

    flake: wip: ixbrl-reporter

--- flake.nix
+++ flake.nix
@@ -66,6 +66,8 @@
   #       };
   #     }
   #   );
+
+
   outputs = inputs@{ parts, ... }: parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: with builtins; {
     systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
     imports = lib.flatten [
@@ -73,9 +75,8 @@
       inputs.my-nix.flakeModules.rust
       (inputs.my-nix.lib.findFlakePartFilesRec ./.)
     ];
-    perSystem = { pkgs, system, ... }:
+    perSystem = { pkgs, l, system, ... }:
       let
-        bin = inputs.my-nix.bin.${system} // (mapAttrs (n: p: "${p}/bin/${n}") scripts);
         buildTimeDeps = [
           pkgs.pkg-config
         ];
@@ -84,7 +85,46 @@
           pkgs.cargo-nextest
         ];
 
+        ownPkgs.ixbrl-reporter =
+          let
+            versions."1.2.1" = { rev = "a0ec37f"; sha256 = "sha256-AMAO3ygDiIVkCsHmHy1fdGp4CVgb7YRV1M8w1mymUhY="; };
+            mkPkg = { version ? l.latest versions, ... }:
+              pkgs.python3Packages.buildPythonPackage rec {
+                pname = "ixbrl-reporter";
+                inherit version;
+                format = "pyproject";
+                src = pkgs.fetchFromGitHub {
+                  owner = "cybermaggedon";
+                  repo = "ixbrl-reporter";
+                  rev = versions.${version}.rev;
+                  sha256 = versions.${version}.sha256;
+                };
+                propagatedBuildInputs = [
+                  pkgs.python3Packages.requests
+                  pkgs.python3Packages.lxml
+                  # pkgs.python3Packages.piecash
+                  pkgs.python3Packages.pyyaml
+                ];
+                nativeBuildInputs = [
+                  pkgs.python3Packages.pytest
+                  pkgs.python3Packages.pytest-cov
+                  pkgs.python3Packages.pytest-mock
+                ];
+                pythonImportsCheck = [ "ixbrl_reporter" ];
+                meta = {
+                  description = "Production of iXBRL reports from templates and accounts files";
+                  homepage = "https://github.com/cybermaggedon/ixbrl-reporter";
+                  license = lib.licenses.gpl3Plus;
+                  platforms = lib.platforms.unix;
+                };
+              };
+          in
+          mkPkg { };
+
         bash.wd = "$(git rev-parse --show-toplevel)";
+        bin = inputs.my-nix.bin.${system} // (mapAttrs (n: p: "${p}/bin/${n}") scripts) // {
+          ixbrl = "${ownPkgs.ixbrl-reporter}/bin/ixbrl-reporter";
+        };
         scripts = mapAttrs (n: s: pkgs.writeShellScriptBin n s) {
           run = ''cargo run -- "$@" '';
           packages = ''if [ -n "$CRATE" ]; then echo "-p $CRATE"; else echo "--workspace"; fi '';
@@ -92,6 +132,7 @@
           utest = ''set -x; cargo nextest run $(packages) -E "''${TEST_FILTER:-all()}" --nocapture "$@" -- $SINGLE_TEST '';
           ftest = ''set -x; cargo nextest run --workspace -E "''${TEST_FILTER:-all()}" --nocapture "$@" '';
           backup-txs = ''mkdir -p ./.cache/backup && mv ./.cache/starling_transactions.json ./.cache/backup/starling_transactions.$(date +%Y%m%d%H%M).json'';
+          report = ''${bin.ixbrl} report/hmrc/corp-tax.yaml report ixbrl > .cache/corp-tax.html'';
         };
 
         env = {
